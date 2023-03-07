62c852ca1410aa2ee36ce92e268e4d17a14103a7
IogaMaster <iogamastercode@gmail.com>
2023-03-07 10:48:40 -0700

    Add rust template

---
+++ rust/flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, ... }:
+  }

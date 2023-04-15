97bc3af360f5035ed85a2accc096a3828c286fdc
IogaMaster <iogamastercode@gmail.com>
2023-04-14 21:44:36 -0600

    Add Go Template

---
+++ go/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Go Template";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          nativeBuildInputs = [
+            go
+            gopls
+          ];
+
+          buildInputs = [ ];
+        };
+      }
+    );
+}

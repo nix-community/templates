e05d97824ea6a7a223bfc0f08133d8ac35e9d60f
IogaMaster <iogamastercode@gmail.com>
2023-04-14 21:31:29 -0600

    Update rust template

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,10 +1,32 @@
 {
-  description = "A devShell example";
+  description = "Rust Template";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+
+    rust-overlay.url = github:oxalica/rust-overlay;
   };
 
-  outputs = { self, nixpkgs, rust-overlay, ... }:
-  }
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          nativeBuildInputs = [
+            rust-bin.stable.latest.default
+            rust-analyzer
+          ];
+
+          buildInputs = [ ];
+        };
+      }
+    );
+}
+

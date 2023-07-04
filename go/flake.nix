b86d930af0c56cba175cc5950e3c9d6c958fe6c6
IogaMaster <iogamastercode@gmail.com>
2023-07-03 22:16:00 -0600

    Add go package build

--- go/flake.nix
+++ go/flake.nix
@@ -20,6 +20,26 @@
 
           buildInputs = with pkgs; [ ];
         };
+
+        packages.default = pkgs.stdenv.mkDerivation rec {
+          name = "go-template";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            go
+          ];
+
+          buildInputs = with pkgs; [ ];
+
+          buildPhase = ''
+            export HOME=$(mktemp -d) # Go needs access to the home directory. So we make a temp directory and set the home to it's path.
+            go build -o ${name}
+          '';
+
+          installPhase = ''
+            cp ${name} $out
+          '';
+        };
       }
     );
 }

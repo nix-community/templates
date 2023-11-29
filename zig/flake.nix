efd20da68d4dfe2559134884508905768c442e45
IogaMaster <iogamastercode@gmail.com>
2023-11-28 20:50:35 -0700

    Update zig template

--- zig/flake.nix
+++ zig/flake.nix
@@ -6,19 +6,35 @@
     flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            zig
-            zls
-          ];
+        nativeBuildInputs = with pkgs; [
+          zig
+          zls
+        ];
 
-          buildInputs = with pkgs; [ ];
+        buildInputs = with pkgs; [];
+      in {
+        devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "template";
+          version = "0.0.0";
+          src = ./.;
+
+          nativeBuildInputs =
+            nativeBuildInputs
+            ++ [
+              pkgs.zig.hook
+            ];
+          inherit buildInputs;
         };
       }
     );

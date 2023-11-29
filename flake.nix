c9108befe6a79336c237736485fd46c2b08d524c
IogaMaster <iogamastercode@gmail.com>
2023-11-28 21:40:16 -0700

    step: Add basic mkWelcomeText function and some uses
    Add more

--- flake.nix
+++ flake.nix
@@ -1,40 +1,92 @@
 {
   description = "Nix Community Templates";
 
-  outputs = {self}: {
-    templates = rec {
-      empty = {
+  outputs = {self}: let
+    mkWelcomeText = {
+      name,
+      description,
+      path,
+      buildTools ? null,
+      setupCommand ? "",
+    }: {
+      inherit path;
+
+      description = name;
+
+      welcomeText = ''
+        # ${name}
+        ${description}
+
+        To set up the project run:
+        ```sh
+            ${setupCommand}
+        ```
+
+        ${
+          if buildTools != null
+          then ''
+            Comes bundled with:
+            ${builtins.concatStringsSep "\n" buildTools}
+          ''
+          else ""
+        }
+
+        ## Other tips
+        If you use direnv run:
+        ```sh
+            echo "use flake" > .envrc
+        ```
+
+        For a quick license setup use licensor:
+        ```sh
+            # SPDX is the license id like MIT or GPL-3.0
+            nix-shell -p licensor --command "licensor <SPDX> > LICENSE"
+        ```
+
+        ## More info
+        - [flake-utils Github Page](https://github.com/numtide/flake-utils)
+      '';
+    };
+  in {
+    templates = {
+      empty = mkWelcomeText {
+        name = "Empty Template";
+        description = ''
+          A simple flake that provides a devshell
+        '';
         path = ./empty;
-        description = "Empty Template";
-        welcomeText = ''
-          # Empty Template
-          Just a simple flake to make setting up a development project with nix a little easier.
-
-          ## More info
-          - [flake-utils Github Page](https://github.com/numtide/flake-utils)
-        '';
       };
-      rust = {
+      rust = mkWelcomeText {
         path = ./rust;
-        description = "Rust Template";
-        welcomeText = ''
-          # Rust Template
+        name = "Rust Template";
+        description = ''
           A basic rust application template with a package build.
-
-          Comes bundled with cargo and rust-analyzer.
-
-          ## More info
-          - [rust-overlay Github Page](https://github.com/oxalica/rust-overlay)
-          - [flake-utils Github Page](https://github.com/numtide/flake-utils)
         '';
+        buildTools = [
+          "The full suite of tools provides by oxalica's [rust-overlay](https://github.com/oxalica/rust-overlay)"
+          "rust-analyzer"
+        ];
       };
-      zig = {
+      zig = mkWelcomeText {
         path = ./zig;
-        description = "Zig Template";
+        name = "Zig Template";
+        description = ''
+          A basic Zig application template with a package build.
+        '';
+        buildTools = [
+          "Zig"
+          "The zig language server"
+        ];
       };
-      go = {
+      go = mkWelcomeText {
         path = ./go;
-        description = "Go Template";
+        name = "Go template";
+        description = "A basic go project";
+        buildTools = [
+          "Go"
+          "gopls language server"
+        ];
+        setupCommand = "go mod init";
       };
       python = {
         path = ./python;
--- go/go.mod
+++
@@ -1,3 +0,0 @@
-module template
-
-go 1.20

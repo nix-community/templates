1362549468fc0a91c691ffdd3f97bfd7ebcca32a
IogaMaster <iogamastercode@gmail.com>
2024-03-31 14:42:12 -0600

    Add julia template

---
+++ julia/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Julia Template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nativeBuildInputs = with pkgs; [
+            (julia.withPackages [
+              "LanguageServer"
+
+              # "Plots"
+              # "PlotThemes"
+            ])
+        ];
+
+        buildInputs = with pkgs; [];
+      in {
+        devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
+      }
+    );
+}

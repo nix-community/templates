a2ae37b9edf77ddad77f655cbe65f6c58cdb6e1f
IogaMaster <iogamastercode@gmail.com>
2026-05-05 21:48:06 -0600

    fix: use new license package
    Fixes #17

--- flake.nix
+++ flake.nix
@@ -45,10 +45,10 @@
 
         For a quick license setup use licensor:
 
-        ```
-            # SPDX is the license id like MIT or GPL-3.0
-            nix-shell -p licensor --command "licensor SPDX > LICENSE"
-        ```
+            ```
+                # SPDX is the license id like MIT or GPL-3.0
+                nix-shell -p license-cli --command "license text SPDX > LICENSE"
+            ```
 
         ## More info
         - [flake-utils Github Page](https://github.com/numtide/flake-utils)

59695f9cb304cc61e810c6d3d7a8cedfe343d2af
Adam Olsson <dagadamsven@gmail.com>
2026-05-09 11:30:48 +0200

    Change name in build.zig

--- zig/build.zig
+++ zig/build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "zig-binary",
+        .name = "template",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
